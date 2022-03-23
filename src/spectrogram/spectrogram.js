cd2bf74db559d92fde66d2cff5cbfa0aa258bbfb
John Leszczynski <jrleszczynski@gmail.com>
2022-03-23 07:41:45 -0700

    Add time axis (initial commit)

--- src/spectrogram/spectrogram.js
+++ src/spectrogram/spectrogram.js
@@ -1,3 +1,4 @@
+import * as d3Axis from 'd3-axis'
 import * as d3Selection from 'd3-selection'
 import * as d3Scale from 'd3-scale'
 import {AudioData} from 'audio-frequency'
@@ -14,6 +15,7 @@ async function getAndDrawData(audioFile) {
     smoothingTimeConstant : 0.8,
   })
   drawSpectrogramData(frequencyData.data)
+  drawSpectrogramAxis(audioData)
   console.log(frequencyData)
 }
 
@@ -29,13 +31,12 @@ function drawSpectrogramData(data, {width = 1400, height = 400} = {}) {
   sonogramCtx.fillStyle = 'rgb(240, 240, 240)'
   sonogramCtx.fillRect(0, 0, width, height)
 
+  // Draw spectrogram
   const decibleColorScale = d3Scale.scaleLinear()
     // getAudioFrequencyData returns a normalized array of values
     // between 0 and 255
     .domain([0, 255])
     .range(['rgba(70, 130, 180, 0)', 'rgba(70, 130, 180, 1.0)'])
-
-  // Draw spectrogram
   console.log('Frequency bin count:', data[0].length)
   console.log('Num samples:', data.length)
   const frequencyBinCount = data[0].length
@@ -53,4 +54,30 @@ function drawSpectrogramData(data, {width = 1400, height = 400} = {}) {
       )
     }
   }
+}
+
+/**
+ * @param {AudioData} audioData
+ */
+function drawSpectrogramAxis(audioData, {width = 1400, height = 400} = {}) {
+  const svg = d3Selection.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    // TODO to change position of axis with respect to chart, specify a transform attribute
+
+  console.log('audioData.duration:', audioData.duration)
+  const timeScale = d3Scale.scaleLinear()
+    .domain([0, audioData.duration])
+    // TODO remove width - 1 once using margin/padding
+    .range([0, width - 1])
+  const timeAxis = d3Axis.axisBottom(timeScale)
+    .ticks(39)
+    // .tickSize(10)
+    // .tickPadding(10)
+
+  svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (10) + ')')
+    .call(timeAxis)
 }
