12b9512a80dd51ab6e831b80d6b2518209300d5c
John Leszczynski <jrleszczynski@gmail.com>
2022-07-04 06:41:19 -0700

    Add sample audio tracks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css'
 import {Spectrogram} from './spectrogram/spectrogram.js'
-import robinSwift from './data/robin-swift.wav'
-import solitaireStero from './data/solitaire-strereo.wav'
-import spottedTowhee from './data/spotted-towhee.wav'
-import towheeSong from './data/towhee-song.wav'
+// import robinSwift from './data/robin-swift.wav'
+import solitaire from './data/solitaire.wav'
+// import spottedTowhee from './data/spotted-towhee.wav'
+// import towheeSong from './data/towhee-song.wav'
+import willow from './data/willow.wav'
 
 
-Spectrogram.fromFile(solitaireStero)
+Spectrogram.fromFile(willow)
