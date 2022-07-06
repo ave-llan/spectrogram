0736b0716ba2753928debf17f779b6a8d0077920
John Leszczynski <jrleszczynski@gmail.com>
2022-07-06 07:10:06 -0700

    Add more samples and ignore unused vars in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars */
 import './style.css'
 import {Spectrogram} from './spectrogram/spectrogram.js'
-// import robinSwift from './data/robin-swift.wav'
+import robinSwift from './data/robin-swift.wav'
+import savannahOptimized from './data/savannah-optimized.wav'
 import solitaire from './data/solitaire.wav'
-// import spottedTowhee from './data/spotted-towhee.wav'
-// import towheeSong from './data/towhee-song.wav'
+import spottedTowhee from './data/spotted-towhee.wav'
+import towheeSong from './data/towhee-song.wav'
 import willow from './data/willow.wav'
+import willowTwoCalls from './data/willow-two-calls.wav'
 
-
-Spectrogram.fromFile(willow)
+Spectrogram.fromFile(willowTwoCalls)
